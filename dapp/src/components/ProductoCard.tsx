8143ca98e7b2727c71cefee9bbabe824f9693a4b
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-08-24 02:30:07 -0600

    Update ProductoCard.tsx

--- dapp/src/components/ProductoCard.tsx
+++ dapp/src/components/ProductoCard.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { Card, CardContent } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "./Card"
+import { Button } from "./Button"
 import { ChevronDown, ChevronUp } from "lucide-react"
 
 export function ProductCard({
